99c579639247fa30215efa807e00bbbada70af7a
Nelia Terrell <nterrell1014@gmail.com>
2023-02-11 23:44:30 -0500

    latest commit

--- src/Methods.java
+++ src/Methods.java
@@ -1,10 +1,20 @@
 public class Methods {
     public static void main(String[] args){
 
-            sayHello();
-    }
+            //sayHello();
 
-    static void sayHello(){
-        System.out.println("Hello");
+        //String greetings = "What's Up";
+        //System.out.println(greetings);
+        greet(); //calling the greet method
     }
+//    static void sayHello(){
+//        //System.out.println("Hello");
+//    }
+    static void greet(){
+        String greetings = "What's Up";
+        System.out.println(greetings);
+}
+
+
+
 }
